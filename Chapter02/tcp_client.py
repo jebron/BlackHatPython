4d61095660cfea63f15b5dfcdc7dd9450890d4e3
jebron <jerrid.brown@gmail.com>
2021-07-03 21:55:49 -0400

    Finished tcp_server.py exercise

--- Chapter02/tcp_client.py
+++ Chapter02/tcp_client.py
@@ -3,13 +3,8 @@ import socket
 target_host = '127.0.0.1'
 target_port = 9998
 
-# create a socket object called client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# call the connect() method and connect the client object
 client.connect((target_host,target_port))
-
-# call the send() method and send some data to the client object
 client.send(b"Test, hello there!\r\n\r\n")
 response = client.recv(4096)
 
--- Chapter02/udp_client.py
+++ Chapter02/udp_client.py
@@ -3,13 +3,8 @@ import socket
 target_host = "127.0.0.1"
 target_port = 9997
 
-# create a socket object called client
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-# using the sendto() method, send some data to the IP and port using the client object
 client.sendto(b"AAABBBCCC",(target_host,target_port))
-
-# put the received data into the variables data and addr
 data, addr = client.recvfrom(4096)
 
 print(data.decode())
